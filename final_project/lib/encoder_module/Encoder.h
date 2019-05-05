642d2d5967d0ade85a8b223e9233465d06b11145
Diego Hamilton <diegohamilton26@gmail.com>
2019-05-05 12:47:06 -0300

    Implement scope of Encoder Initialize main thread The module was tested with event signals

--- final_project/lib/encoder_module/Encoder.h
+++ final_project/lib/encoder_module/Encoder.h
@@ -1,17 +1,34 @@
+/* Final project for Automotive Networks Course at CIn, UFPE.
+ *
+ * Professor: Divanilson Campello.
+ * Teaching Assistants:
+ *  Paulo Freitas de Araújo Filho
+ *  Saulo Vinicius Ferreira Barreto
+ *  Nathan Martins Freire 
+ *
+ * Encoder Module for a CAN controller.
+ *
+ * Authors:
+ *  Diego Hamilton
+ *  Lucas Cavalcanti
+ * 
+ * Microcontroller: NUCLEO-STM32F303RE
+ * Developed with the ARM Mbed IDE. 
+ */
+
 #ifndef ENCODER_H
 #define ENCODER_H
 
-#ifndef MBED_H
-#define MBED_H
 #include "mbed.h"
-#endif
+#include "EventDefs.h"
+
 
 /* Frame Interpreter Machine States */
 typedef enum
 {
     TRANSMITTING_ST,
-    ERROR_HANDLER_ST,
-    IDLE_ST,
+    ERROR_HANDLER_ENC_ST,
+    IDLE_ENC_ST,
     /* TRANSMITTING states */
     FRAME_CHECK_ST,
     CRC_ST,                 // calculate cyclic redundancy check
@@ -23,14 +40,21 @@ typedef enum
 } encoder_states_t;
 
 /* Global variables */
+#ifndef SHARED_EVENTS
+#define SHARED_EVENTS
+extern Timer timer; 
 extern EventFlags shared_events;
+#endif
 
 class Encoder
 {
     public:
-
+        Encoder();
     private:
-        
+        /* Private methods */
+        void execute();                     // state machine of Frame Interpreter Module
+        /* Private atributes */
+        Thread exe;                         // execution thread
 };
 
 #endif
